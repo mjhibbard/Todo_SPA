e823b92a8deb9041b891648bfcd103c4c3b7d868
Michael Hibbard <38772539+mjhibbard@users.noreply.github.com>
2018-06-17 20:33:36 -0700

    setup router, routes, mongoose and models

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-var express = require("express"),
-    app     = express(),
+var express     = require("express"),
+    app         = express(),
     bodyParser  = require("body-parser"),
-    port    = process.env.PORT || 3000;
+    todoRoutes  = require("./routes/todos");
 
-app.get("/", function (){
+
+//App Config
+app.use("/api/todos", todoRoutes);
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res){
     res.send("Hello and welcome.");
 });
 
-app.listen(port, function(){
+app.listen(3000, function(){
     console.log("Todo App is Listening!!");
 });
+
+//Start the MongoDB environment
+//"C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" --dbpath "C:\Users\Mike\Documents\Alpha-Code\Adv WebDev\SPA Todo App\data"
+
+
+//Start MongoDB
+//"C:\Program Files\MongoDB\Server\3.6\bin\mongo.exe"
